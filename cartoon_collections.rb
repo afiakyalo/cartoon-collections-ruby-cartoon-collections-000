eeacb60633a1928cff5c501b2b03695c548910b5
Afia Kyalo <afiakyalo@gmail.com>
2019-07-07 00:03:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarves)
 end 
 
 def summon_captain_planet(array)
-  array.map { |c| c.capitalize }
+  array.map! { |c| c.capitalize }
+  
 end
 
 def long_planeteer_calls# code an argument here
