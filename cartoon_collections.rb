cba1ed52f44339c3570de66cf2fde03c98b73fb3
Afia Kyalo <afiakyalo@gmail.com>
2019-07-07 00:10:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,11 @@ def summon_captain_planet(array)
   array.collect { |c| c + "!" }
 end
 
-def long_planeteer_calls(calls)
-  
-end
+def long_planeteer_calls(array)
+  array.any? do |word| 
+  word.length > 4 
+  end 
+end 
 
 def find_the_cheese# code an argument here
   # the array below is here to help
